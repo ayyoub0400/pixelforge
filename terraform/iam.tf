da3ae775e0c3ebb4a1c184821524b76d78f65329
ayyoub0400 <ayoubmimoune9@gmail.com>
2026-08-19 19:22:55 +0100

    Added IAM Roles and Policies

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -131,3 +131,39 @@ data "aws_iam_policy_document" "worker" {
 
   }
 }
+
+
+
+resource "aws_iam_role" "api" {
+
+  name = "${var.project_name}-${var.environment}-api"
+  assume_role_policy = data.aws_iam_policy_document.assume_from_account.json
+
+}
+
+#the role and what it can assume
+resource "aws_iam_role" "worker" {
+
+  name = "${var.project_name}-${var.environment}-worker"
+  assume_role_policy = data.aws_iam_policy_document.assume_from_account.json
+
+}
+
+#what our created role can do --- role + policy ---
+resource "aws_iam_role_policy" "api" {
+
+  name = "${var.project_name}-${var.environment}-api"
+  role = aws_iam_role.api.id
+  policy = data.aws_iam_policy_document.api.json
+
+}
+
+resource "aws_iam_role_policy" "worker" {
+
+  name = "${var.project_name}-${var.environment}-worker"
+  role = aws_iam_role.worker.id
+  policy = data.aws_iam_policy_document.worker.json
+
+}
+
+
