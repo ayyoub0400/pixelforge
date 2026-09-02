ba7c148d3f305e969258a14396d42ec0a9cec2a8
ayyoub0400 <ayoubmimoune9@gmail.com>
2026-09-02 20:25:54 +0100

    Added EKS Node group

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -81,3 +81,81 @@ resource "aws_eks_access_policy_association" "self_admin" {
   }
 
 }
+
+
+
+
+### eks node group ###
+
+
+data "aws_iam_policy_document" "node_assume" {
+
+  statement {
+  
+    actions = ["sts:AssumeRole"]
+    principals {
+
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    
+    }
+
+  }
+
+}
+
+resource "aws_iam_role" "node" {
+
+  name = "${var.project_name}-${var.environment}-node"
+  assume_role_policy = data.aws_iam_policy_document.node_assume.json
+
+}
+
+#workernodepolicy - allow to join eks cluster
+#cni for networking/Private IPs
+#ECR role to grab images
+
+resource "aws_iam_role_policy_attachment" "node_worker" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+resource "aws_iam_role_policy_attachment" "node_cni" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+resource "aws_iam_role_policy_attachment" "node_ecr" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
+#the actual node group
+
+resource "aws_eks_node_group" "default" {
+
+  cluster_name = aws_eks_cluster.this.name
+  node_group_name = "${var.project_name}-${var.environment}-default"
+  node_role_arn = aws_iam_role.node.arn
+
+  subnet_ids = module.vpc.private_subnets
+
+
+  #going to be handled by KEDA later
+  scaling_config {
+
+    desired_size = 2
+    min_size = 1
+    max_size = 3
+
+  }
+
+  instance_types = ["t3.medium"]
+  capacity_type = "ON_DEMAND"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_worker,
+    aws_iam_role_policy_attachment.node_cni,
+    aws_iam_role_policy_attachment.node_ecr,
+  ]
+
+}
