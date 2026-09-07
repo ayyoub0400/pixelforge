077a298cc5ba0ef7a319fd813498a4cc11430d26
ayyoub0400 <ayoubmimoune9@gmail.com>
2026-09-07 18:08:48 +0100

    Amended worker deployment liveness check path

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -15,3 +15,18 @@ TABLE_ARN=$(terraform output -raw dynamodb_table_arn)
 API_ROLE=$(terraform output -raw api_role_arn)
 WORKER_ROLE=$(terraform output -raw worker_role_arn)
 CI_ROLE=$(terraform output -raw ci_role_arn)
+
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 266735805454.dkr.ecr.eu-west-2.amazonaws.com
+
+sleep 3 
+
+docker build -f ../docker/Dockerfile.api -t 266735805454.dkr.ecr.eu-west-2.amazonaws.com/pixelforge/api:dev
+docker push 266735805454.dkr.ecr.eu-west-2.amazonaws.com/pixelforge/api:dev
+
+sleep 3
+
+docker build -f ../docker/Dockerfile.worker -t 266735805454.dkr.ecr.eu-west-2.amazonaws.com/pixelforge/worker:dev
+docker push 266735805454.dkr.ecr.eu-west-2.amazonaws.com/pixelforge/worker:dev
+
+kubectl apply -f ../k8s/serviceaccounts.yaml
+kubectl apply -f ../k8s/
